2f04669449fb4eed922d7af21a2ac31759c1d076
SLagardeJFC <sylvain.lagarde1@univ-tlse3.fr>
2020-12-02 08:50:10 +0100

    This closes #6 .

--- src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/LoginGenerator.java
@@ -35,7 +35,16 @@ public class LoginGenerator {
      */
     public String generateLoginForNomAndPrenom(String nom, String prenom) {
         String p = deAccent(prenom.substring(0,1).toUpperCase());
-        String n = deAccent(nom.substring(0,3).toUpperCase());
+        String n;
+        try {
+            n = deAccent(nom.substring(0, 3).toUpperCase());
+        } catch (Exception e) {
+            try {
+                n = deAccent(nom.substring(0, 2).toUpperCase());
+            } catch (Exception ex) {
+                n = deAccent(nom.substring(0, 1).toUpperCase());
+            }
+        }
         String login = p+n ;
         if (loginService.loginExists(login)) {
             login = login + "1" ;
